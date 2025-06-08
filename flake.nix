db6d17f322694ff03cb6e52609387a7270f79e78
Mohamed Gaber <me@donn.website>
2025-06-08 20:21:31 +0300

    nix: update nix-eda

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
   description = "open-source infrastructure for implementing chip design flows";
 
   inputs = {
-    nix-eda.url = "github:fossi-foundation/nix-eda/2.1.2";
+    nix-eda.url = "github:fossi-foundation/nix-eda/2.1.3";
     libparse.url = "github:efabless/libparse-python";
     ciel.url = "github:fossi-foundation/ciel";
     devshell.url = "github:numtide/devshell";
