4a67e1d475b693db25f9484c547b9b66dc3cad08
Leo Moser <leomoser99@gmail.com>
2025-07-18 09:27:05 +0200

    fixup

--- .github/scripts/get_pdk_hash.py
+++ .github/scripts/get_pdk_hash.py
@@ -18,9 +18,10 @@ import sys
 import yaml
 import click
 
+
 @click.command()
-@click.argument('filename')
-@click.option('--pdk-family', help='The PDK family for which to get the hash.')
+@click.argument("filename")
+@click.option("--pdk-family", help="The PDK family for which to get the hash.")
 def get_pdk_hash(filename, pdk_family):
     """
     Prints the hash of the PDK family in filename.
@@ -28,11 +29,12 @@ def get_pdk_hash(filename, pdk_family):
 
     with open(filename, "r") as file:
         pdk_hashes = yaml.safe_load(file)
-        
+
         if pdk_family in pdk_hashes:
             print(pdk_hashes[pdk_family])
         else:
             sys.exit(1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     get_pdk_hash()
