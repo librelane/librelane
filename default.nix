librelane/librelane

commit 6917884cadd840b4fa32c0884d6c00db74e6e183

feat: update nix-eda, openroad, replace synlig with slang (#702)
This CI updates nix-eda and its included tools, and OpenROAD.

Some updates to scripts have taken place due to API breaks and tweaks in
the relevant utilities (as usual.)

Additionally, as the synlig plugin for Yosys is under-maintained, we
chose to switch to yosys-slang which I am aware is under active
maintenance (also, it's a lot easier to build.)

default.nix
@@ -23,7 +23,7 @@
   fetchFromGitHub,
   nix-gitignore,
   # Tools
-  klayout,
+  klayout-app,
   libparse,
   magic-vlsi,
   netgen,
@@ -36,7 +36,11 @@
   verilog,
   ciel,
   yosys,
-  yosysFull,
+  yosys-sby,
+  yosys-eqy,
+  yosys-lighter,
+  yosys-slang,
+  yosys-ghdl,
   # Python
   buildPythonPackage,
   poetry-core,
@@ -56,11 +60,18 @@
   pyfakefs,
   rapidfuzz,
   semver,
+  klayout,
 }: let
-  yosys-env = (yosys.withPythonPackages.override {target = yosysFull;}) (ps:
-    with ps; [
-      click
-    ]);
+  yosys-with-plugins = yosys.withPlugins (
+    [
+      yosys-sby
+      yosys-eqy
+      yosys-lighter
+      yosys-slang
+    ]
+    ++ lib.optionals (lib.lists.any (el: el == system) yosys-ghdl.meta.platforms) [yosys-ghdl]
+  );
+  yosys-env = (yosys.withPythonPackages.override {target = yosys-with-plugins;}) (ps: with ps; [click]);
   openroad-env = openroad.withPythonPackages (ps:
     with ps; [
       click
@@ -72,7 +83,10 @@
     version = (builtins.fromTOML (builtins.readFile ./pyproject.toml)).tool.poetry.version;
     format = "pyproject";
 
-    src = if (flake != null) then flake else nix-gitignore.gitignoreSourcePure ./.gitignore ./.;
+    src =
+      if (flake != null)
+      then flake
+      else nix-gitignore.gitignoreSourcePure ./.gitignore ./.;
 
     nativeBuildInputs = [
       poetry-core
@@ -82,9 +96,9 @@
       opensta
       yosys-env
       openroad-env
-      klayout
       netgen
       magic-vlsi
+      klayout-app
       verilog
       verilator
       tclFull
@@ -92,27 +106,27 @@
       ruby
     ];
 
-    propagatedBuildInputs =
-      [
-        # Python
-        click
-        cloup
-        pyyaml
-        yamlcore
-        rich
-        requests
-        pcpp
-        ciel
-        tkinter
-        lxml
-        deprecated
-        libparse
-        psutil
-        klayout.pymod
-        rapidfuzz
-        semver
-      ]
-      ++ self.includedTools;
+    propagatedBuildInputs = self.includedTools;
+
+    dependencies = [
+      # Python
+      click
+      cloup
+      pyyaml
+      yamlcore
+      rich
+      requests
+      pcpp
+      ciel
+      tkinter
+      lxml
+      deprecated
+      libparse
+      psutil
+      klayout
+      rapidfuzz
+      semver
+    ];
 
     doCheck = true;
     checkInputs = [pytestCheckHook pytest-xdist pyfakefs];

.github/workflows/ci.yml
@@ -66,9 +66,6 @@ jobs:
         run: |
           export GITHUB_TOKEN=${{ secrets.GITHUB_TOKEN }}
           ciel enable --pdk gf180mcu --pdk-root ./.ciel-gf180mcu ${{ steps.set-rev.outputs.opdks_rev }}
-      - name: Download IPM
-        run: |
-          python3 -m pip install git+https://github.com/efabless/IPM
       - name: Checkout submodules
         uses: actions/checkout@v4
         with:
@@ -230,7 +227,7 @@ jobs:
       - name: Cache to S3
         uses: fossi-foundation/nix-eda/.github/actions/nix_sign_cache_s3@main
         with:
-          flake_outputs: .#packages.${{ matrix.os. nix}}.librelane
+          flake_outputs: .#packages.${{ matrix.os.nix }}.librelane
           nix_public_key: ${{ vars.NIX_PUBLIC_KEY }}
           nix_private_key: ${{ secrets.NIX_PRIVATE_KEY }}
           s3_bucket_name: ${{ vars.NIX_CACHE }}
@@ -363,20 +360,19 @@ jobs:
       - name: Enable PDKs
         if: steps.cache-pdk.outputs.cache-hit != 'true'
         run: |
-          pip3 install poetry poetry-plugin-export
-          poetry export --with dev --without-hashes --format=requirements.txt --output=requirements_tmp.txt
-          pip3 install -r requirements_tmp.txt
-          ciel enable --pdk ${{ matrix.design.pdk_family }} \
+          pipx run ciel==$(perl -ne 'print /version\s*=\s*"([\d\.]+)"/ if $c; $c = /name\s*=\s*"ciel"/' ./poetry.lock )\
+            -- enable --pdk ${{ matrix.design.pdk_family }} \
             --pdk-root ./.ciel-${{ matrix.design.pdk_family }} \
             ${{ needs.prepare-test-matrices.outputs.opdks_rev }}
-
       - name: Download IPM designs
         id: download-ipm-design
         if: matrix.design.ipm_version != 'None'
         run: |
           export GITHUB_TOKEN=${{ secrets.GITHUB_TOKEN }}
-          python3 -m pip install git+https://github.com/efabless/IPM
-          ipm install --ip-root $(realpath $IP_ROOT) --ipm-root ~/.ipm --version ${{ matrix.design.ipm_version }} ${{ matrix.design.name }}
+          pipx run ipmgr \
+            -- install --ip-root $(realpath $IP_ROOT) \
+            --ipm-root ~/.ipm --version ${{ matrix.design.ipm_version }} \
+            ${{ matrix.design.name }}
 
       - name: Run Test
         id: test_run
@@ -498,6 +494,14 @@ jobs:
     needs: [build-docker, build-py]
     name: Publish (If Applicable)
     steps:
+      - name: Maximize build space
+        uses: AdityaGarg8/remove-unwanted-software@v5
+        with:
+          remove-dotnet: "true"
+          remove-android: "true"
+          remove-haskell: "true"
+          remove-codeql: "true"
+          remove-docker-images: "true"
       - name: Check out repo
         uses: actions/checkout@v4
         with:
@@ -552,7 +556,7 @@ jobs:
           make dist
       - name: Publish
         if: ${{ env.PUBLISH == '1' }}
-        uses: pypa/gh-action-pypi-publish@master
+        uses: pypa/gh-action-pypi-publish@release/v1
         with:
           password: ${{ secrets.PYPI_API_TOKEN }}
       - name: Tag Commit

flake.nix
@@ -19,7 +19,7 @@
   description = "open-source infrastructure for implementing chip design flows";
 
   inputs = {
-    nix-eda.url = "github:fossi-foundation/nix-eda/4.3.0";
+    nix-eda.url = "github:fossi-foundation/nix-eda/donn/nixos_2505";
     libparse.url = "github:efabless/libparse-python";
     ciel.url = "github:fossi-foundation/ciel";
     devshell.url = "github:numtide/devshell";
@@ -46,22 +46,11 @@
       default = lib.composeManyExtensions [
         (import ./nix/overlay.nix)
         (nix-eda.flakesToOverlay [libparse ciel])
-        (pkgs': pkgs: {
-          yosys-sby = (pkgs.yosys-sby.override { sha256 = "sha256-Il2pXw2doaoZrVme2p0dSUUa8dCQtJJrmYitn1MkTD4="; });
-          klayout = (pkgs.klayout.overrideAttrs(old: {
-            configurePhase = builtins.replaceStrings ["-without-qtbinding"] ["-with-qtbinding"] old.configurePhase;
-          }));
-          yosys = pkgs.yosys.overrideAttrs(old: {
-            patches = old.patches ++ [
-              ./nix/patches/yosys/async_rules.patch
-            ];
-          });
-        })
         (
           pkgs': pkgs: let
             callPackage = lib.callPackageWith pkgs';
           in {
-            or-tools_9_11 = callPackage ./nix/or-tools_9_11.nix {
+            or-tools_9_14 = callPackage ./nix/or-tools_9_14.nix {
               inherit (pkgs'.darwin) DarwinTools;
               stdenv =
                 if pkgs'.system == "x86_64-darwin"
@@ -71,18 +60,32 @@
             colab-env = callPackage ./nix/colab-env.nix {};
             opensta = callPackage ./nix/opensta.nix {};
             openroad-abc = callPackage ./nix/openroad-abc.nix {};
-            openroad = callPackage ./nix/openroad.nix {};
+            openroad = callPackage ./nix/openroad.nix {
+              llvmPackages = pkgs'.llvmPackages_18;
+            };
           }
         )
         (
           nix-eda.composePythonOverlay (pkgs': pkgs: pypkgs': pypkgs: let
             callPythonPackage = lib.callPackageWith (pkgs' // pkgs'.python3.pkgs);
           in {
-            mdformat = pypkgs.mdformat.overridePythonAttrs (old: {
+            mdformat = pypkgs.mdformat.overridePythonAttrs {
+              version = "0.7.18";
+              src = pypkgs'.fetchPypi {
+                pname = "mdformat";
+                version = "0.7.18";
+                hash = "sha256-QsuovFprsS1QvffB5HDB+Deoq4zoFXHU5TueYgUfbk8=";
+              };
+
               patches = [
                 ./nix/patches/mdformat/donns_tweaks.patch
               ];
+
               doCheck = false;
+            };
+            ciel = pkgs.ciel.overrideAttrs (attrs': attrs: {
+              buildInputs = attrs.buildInputs ++ [pypkgs'.pythonRelaxDepsHook];
+              pythonRelaxDeps = ["rich"];
             });
             sphinx-tippy = callPythonPackage ./nix/sphinx-tippy.nix {};
             sphinx-subfigure = callPythonPackage ./nix/sphinx-subfigure.nix {};
@@ -121,8 +124,9 @@
 
     packages = nix-eda.forAllSystems (
       system: let
-        pkgs = (self.legacyPackages."${system}");
-        in {
+        pkgs = self.legacyPackages."${system}";
+      in
+        {
           inherit (pkgs) colab-env opensta openroad-abc openroad;
           inherit (pkgs.python3.pkgs) librelane;
           default = pkgs.python3.pkgs.librelane;
@@ -132,8 +136,7 @@
         }
     );
 
-    # devshells
-
+    # dev
     devShells = nix-eda.forAllSystems (
       system: let
         pkgs = self.legacyPackages."${system}";
@@ -171,6 +174,7 @@
             types-pyyaml
             types-psutil
             lxml-stubs
+            pipx
           ];
           include-librelane = false;
         }) {};

librelane/config/__main__.py
@@ -14,7 +14,6 @@
 import os
 import sys
 import json
-import functools
 from decimal import Decimal
 
 import click
@@ -111,9 +110,7 @@ def create_config(
                 print("At least one source RTL file is required.", file=sys.stderr)
                 exit(1)
     source_rtl_key = "VERILOG_FILES"
-    if not functools.reduce(
-        lambda acc, x: acc and (x.endswith(".sv") or x.endswith(".v")), source_rtl, True
-    ):
+    if not all((file.endswith(".sv") or file.endswith(".v")) for file in source_rtl):
         print(
             "Only Verilog/SystemVerilog files are supported by create-config.",
             file=sys.stderr,

librelane/config/removals.py
@@ -42,4 +42,5 @@ removed_variables: Dict[str, str] = {
     "FP_PADFRAME_CFG": "To be implemented.",
     "FP_CONTEXT_DEF": "To be implemented.",
     "FP_CONTEXT_LEF": "To be implemented.",
+    "SYNLIG_DEFER": "Synlig replaced with Slang.",
 }

librelane/config/variable.py
@@ -575,7 +575,7 @@ class Variable:
             kwargs_dict = {}
             for current_field in fields(validating_type):
                 key = current_field.name
-                subtype = current_field.type
+                subtype: Type[Any] = current_field.type  # type: ignore
                 explicitly_specified = False
                 if key in raw:
                     explicitly_specified = True

librelane/scripts/odbpy/reader.py
@@ -166,7 +166,7 @@ class OdbReader(object):
         grt.setMinLayerForClock(min_clk_idx)
         grt.setMaxLayerForClock(max_clk_idx)
         grt.setMacroExtension(self.config["GRT_MACRO_EXTENSION"])
-        grt.setOverflowIterations(self.config["GRT_OVERFLOW_ITERS"])
+        grt.setCongestionIterations(self.config["GRT_OVERFLOW_ITERS"])
         grt.setAllowCongestion(self.config["GRT_ALLOW_CONGESTION"])
         grt.setVerbose(True)
         grt.initFastRoute(min_layer_idx, max_layer_idx)

librelane/scripts/openroad/common/io.tcl
@@ -174,9 +174,9 @@ proc read_timing_info {args} {
     foreach nl $::env(_CURRENT_CORNER_NETLISTS) {
         puts "Reading macro netlist at '$nl'…"
         if { [catch {read_verilog $nl} err] } {
-            puts "Error while reading macro netlist '$nl':"
-            puts $err
-            puts "Make sure that this a gate-level netlist and not an RTL file."
+            puts stderr "Error while reading macro netlist '$nl':"
+            puts stderr $err
+            puts stderr "Make sure that this a gate-level netlist and not an RTL file."
             exit 1
         }
     }
@@ -185,9 +185,9 @@ proc read_timing_info {args} {
             if { [string_in_file $verilog_file $blackbox_wildcard] } {
                 puts "Found '$blackbox_wildcard' in '$verilog_file', skipping…"
             } elseif { [catch {puts "Reading Verilog model at '$verilog_file'…"; read_verilog $verilog_file} err] } {
-                puts "Error while reading $verilog_file:"
-                puts $err
-                puts "Make sure that this a gate-level netlist and not an RTL file, otherwise, you can add the following comment '$blackbox_wildcard' in the file to skip it and blackbox the modules inside if needed."
+                puts stderr "Error while reading $verilog_file:"
+                puts stderr $err
+                puts stderr "Make sure that this a gate-level netlist and not an RTL file, otherwise, you can add the following comment '$blackbox_wildcard' in the file to skip it and blackbox the modules inside if needed."
                 exit 1
             }
         }
@@ -411,6 +411,13 @@ proc write_views {args} {
         write_verilog $::env(SAVE_NL)
     }
 
+    if { [info exists ::env(SAVE_LOGICAL_NL)] } {
+        puts "Writing logic-only netlist to '$::env(SAVE_LOGICAL_NL)'…"
+        write_verilog\
+            -remove_cells "[get_physical_cells]"\
+            $::env(SAVE_LOGICAL_NL)
+    }
+
     if { [info exists ::env(SAVE_PNL)] } {
         puts "Writing powered netlist to '$::env(SAVE_PNL)'…"
         write_verilog -include_pwr_gnd $::env(SAVE_PNL)
@@ -575,7 +582,8 @@ proc get_layers {args} {
         flags {-constrained}
 
     if { ![info exists keys(-types)] } {
-        puts "\[ERROR\] Invalid usage of get_layers: -types is required."
+        puts stderr "\[ERROR\] Invalid usage of get_layers: -types is required."
+        return -code error
     }
 
     set layers [$::tech getLayers]

commit a3c5c5ae213dfb0debb562c2d8407b544d2a3906

hotfix: containers not working with opensta
regression introduced in 6917884cadd840b4fa32c0884d6c00db74e6e183 most likely

default.nix
@@ -131,7 +131,10 @@
     doCheck = true;
     checkInputs = [pytestCheckHook pytest-xdist pyfakefs];
 
-    computed_PATH = lib.makeBinPath self.propagatedBuildInputs;
+    computed_PATH =
+      "${opensta.out}:"
+      + # HACK: for whatever reason, opensta.dev is prioritized here.
+      (lib.makeBinPath self.propagatedBuildInputs);
 
     # Make PATH available to LibreLane subprocesses
     makeWrapperArgs = [

nix/docker.nix
@@ -58,7 +58,7 @@ in
       mkdir -p ./etc
       mkdir -p ./tmp
       chmod 1777 ./tmp
-      
+
       cat <<HEREDOC > ./etc/zshrc
       autoload -U compinit && compinit
       autoload -U promptinit && promptinit && prompt suse && setopt prompt_sp