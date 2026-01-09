27fff35eeae4c1b1e769f3863edc12ae679eb922
Gon Solo <gonsolo@gmail.com>
2026-01-09 16:07:37 +0100

    Fix call.

--- type_stubs/libyosys.pyi
+++ type_stubs/libyosys.pyi
@@ -24,6 +24,4 @@ class Design:
 
 class Pass:
     @staticmethod
-    def call__YOSYS_NAMESPACE_RTLIL_Design__std_vector_string_(
-        design: Design, cmd: List[str]
-    ): ...
+    def call(design: Design, cmd: List[str]): ...
