58bb2e2d5b511888e0db3eb2f453d69604b42e95
Mohamed Gaber <me@donn.website>
2026-03-06 05:13:20 +0200

    chore: bump nix-eda to 6.7.0
    to pull in magic update to 8.3.615

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   description = "open-source infrastructure for implementing chip design flows";
 
   inputs = {
-    nix-eda.url = "github:fossi-foundation/nix-eda/6.5.0";
+    nix-eda.url = "github:fossi-foundation/nix-eda/6.7.0";
     ciel.url = "github:fossi-foundation/ciel";
     devshell.url = "github:numtide/devshell";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
