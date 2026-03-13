b44bfb47e27cfa84c0a1899afbf01804fc1e0f7d
Leo Moser <leomoser99@gmail.com>
2026-03-13 08:06:45 +0100

    feat: magic: improve get_bbox, update nix-eda (#887)
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

--- librelane/scripts/magic/get_bbox.tcl
+++ librelane/scripts/magic/get_bbox.tcl
@@ -1,11 +1,12 @@
 gds read $::env(_GDS_IN)
 load $::env(_MACRO_NAME_IN)
-set properties [property]
-foreach property [property] {
-    if {[lindex $property 0] == "FIXED_BBOX"} {
-        puts "%OL_METRIC_I llx [lindex $property 1]"
-        puts "%OL_METRIC_I lly [lindex $property 2]"
-        puts "%OL_METRIC_I urx [lindex $property 3]"
-        puts "%OL_METRIC_I ury [lindex $property 4]"
-    }
+set curunits [units]
+units internal
+set bbox [property list FIXED_BBOX]
+if {$bbox != {}} {
+    puts "%OL_METRIC_I llx [lindex $bbox 0]"
+    puts "%OL_METRIC_I lly [lindex $bbox 1]"
+    puts "%OL_METRIC_I urx [lindex $bbox 2]"
+    puts "%OL_METRIC_I ury [lindex $bbox 3]"
 }
+units {*}$curunits
