efca21849bc40a43dc9e2da3634c68b9b4e1287a
Noam Cohen <noam.chn1@gmail.com>
2026-09-08 22:56:12 +0200

    fix: adapt main Nix configuration for KeplerFormal dependencies
    Align nixpkgs with nix-eda 7.1, remove obsolete package overrides, use the relocated tclint package, and retain platform availability checks for yosys-ghdl. Allow the bundled Click 8.3 without changing existing locked Python packages.

--- default.nix
+++ default.nix
@@ -30,9 +30,7 @@
     yosys-eqy
     yosys-slang
   ]
-  ++ lib.optionals (lib.lists.any (
-    el: el == clangStdenv.hostPlatform.system
-  ) yosys-ghdl.meta.platforms) [ yosys-ghdl ],
+  ++ lib.optionals (lib.meta.availableOn clangStdenv.hostPlatform yosys-ghdl) [ yosys-ghdl ],
   extra-yosys-plugins ? [ ],
   # Python
   buildPythonPackage,
--- flake.nix
+++ flake.nix
@@ -43,16 +43,7 @@
               colab-env = callPackage ./nix/colab-env.nix { };
               opensta = callPackage ./nix/opensta.nix { };
               openroad-abc = callPackage ./nix/openroad-abc.nix { };
-              openroad = callPackage ./nix/openroad.nix {
-                llvmPackages = pkgs'.llvmPackages_18;
-              };
-              lemon-graph = pkgs.lemon-graph.overrideAttrs (
-                finalAttrs: previousAttrs: {
-                  patches = previousAttrs.patches ++ [
-                    ./nix/patches/lemon-graph/update_cxx20.patch
-                  ];
-                }
-              );
+              openroad = callPackage ./nix/openroad.nix { };
             }
           )
           (nix-eda.composePythonOverlay (
@@ -63,19 +54,8 @@
             {
               libparse = callPythonPackage ./nix/libparse.nix { };
 
-              # warning with every single click invocation
-              cloup = pypkgs.cloup.overridePythonAttrs {
-                postPatch = ''
-                  substituteInPlace cloup/_util.py \
-                    --replace-fail \
-                      "tuple(click.__version__.split('.'))" \
-                      "tuple('${pypkgs'.click.version}'.split('.'))"
-                '';
-              };
-
               sphinx-tippy = callPythonPackage ./nix/sphinx-tippy.nix { };
               sphinx-subfigure = callPythonPackage ./nix/sphinx-subfigure.nix { };
-              yamlcore = callPythonPackage ./nix/yamlcore.nix { };
               py-mon = callPythonPackage ./nix/py-mon.nix { };
 
               # ---
