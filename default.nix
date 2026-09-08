4b8760955988ecb49519bfe2bcb5ebb720647c57
Leo Moser <leomoser99@gmail.com>
2026-09-08 20:20:42 +0200

    feat: update Ciel and enable ihp-sg13cmos5l in test sets
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

--- default.nix
+++ default.nix
@@ -109,6 +109,11 @@ let
       rapidfuzz
       semver
     ];
+    
+    postPatch = ''
+      substituteInPlace pyproject.toml \
+        --replace-fail "  \"ciel>=2.3.1,<3\"," "" \
+    '';
 
     doCheck = true;
     checkInputs = [
